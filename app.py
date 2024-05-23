c5ba9512133842a54929bd016e843ec6fba1edc1
joseph <joseph.soper.dev@outlook.com>
2024-05-23 04:05:30 -0400

    moved home to app.py and setup gunicorn

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 ''' My Portfolio App '''
-from flask import Flask
+from flask import Flask, render_template, Blueprint
 from home import home
 
 app = Flask(__name__)
-app.register_blueprint(home, url_prefix="/home")
+app.register_blueprint(home, url_prefix="/")
+
+home = Blueprint(__name__, "home")
+
+@home.route("/")
+def home_page():
+    ''' return home page '''
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
